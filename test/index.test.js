ad8825a47b754325a979e88e663252460e96869c
Fred Snyder <fsnyder@castwide.com>
2019-05-02 04:15:04 -0400

    Require opal test.

--- test/index.test.js
+++ test/index.test.js
@@ -2,17 +2,24 @@ import compiler from './fixtures/compiler.js';
 import path from 'path';
 
 test('Compiles a simple Ruby script', async () => {
-  jest.setTimeout(10000);
   const stats = await compiler('simple.rb');
   const output = stats.toJson().modules[0].source;
-  expect(output).toContain('Hello, world!')
+  expect(output).toContain('Hello, world!');
 });
 
 test('Compiles with a custom load path', async () => {
-  jest.setTimeout(10000);
   const stats = await compiler('load.rb', {
     paths: [path.resolve(__dirname, 'fixtures')]
   });
   const output = stats.toJson().modules[0].source;
-  expect(output).toContain('Required script')
+  expect(output).toContain('Required script');
+});
+
+test('Compiles with Opal', async () => {
+  jest.setTimeout(10000);
+  const stats = await compiler('opal.rb', {
+    paths: [path.resolve(__dirname, 'fixtures')]
+  });
+  const output = stats.toJson().modules[0].source;
+  expect(output).toContain('Hello, Opal!');
 });
